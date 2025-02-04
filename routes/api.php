5903bf66066a212aa2e279bb0f173ce1430a4ab2
Haibikk <viagashow@gmail.com>
2025-02-04 20:55:18 +0700

    Upgrade laravel 10 into laravel 11

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::apiResource('users', UserController::class);
+Route::apiResource('categories', CategoryController::class);
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
